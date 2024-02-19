751655ba7669f3911e0ede6d144843aeb5244b0b
Allan Gonçalves da Silva <allan.goncalves@elements.com.br>
2024-02-19 11:45:14 -0300

    Teste com mesa protitipo usando os reles e arduino escravo, encoder em teste manual

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,122 @@
 #include <Wire.h>
 #include <Adafruit_MPR121.h>
 #include <U8g2lib.h>
+#include <ESP32Encoder.h>
+
+// Definindo os pinos do encoder
+#define ENCODER_CLK_PIN 27  // Pino CLK do encoder
+#define ENCODER_DT_PIN 26   // Pino DT do encoder
+
+// Criando uma instância do encoder
+ESP32Encoder encoder;
 
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 Adafruit_MPR121 touch;
-unsigned int variavel = 90;
+unsigned int altura = 90;
 uint16_t lastTouched = 0;
-bool portaTocadaAnteriormente[12] = {false};  // Array para rastrear o estado anterior de cada porta
+bool portaTocadaAnteriormente[12] = {false};
 unsigned long ultimaAlteracao = 0;
-bool tela = true;
+unsigned long desligaTela = 0;
+int tela = 0;
+
+long encoderValue;
 
 void draw(void) {
-    u8g2.setFont(u8g2_font_inb30_mr);
-    
-    // Centraliza o valor da variável no meio da tela
-    String variavelStr = String(variavel);
-    const char* variavelChar = variavelStr.c_str();
+    if (tela == 0){
+      u8g2.setFont(u8g2_font_inb30_mr);
+      // Centraliza o valor da variável no meio da tela
+      String variavelStr = String(altura);
+      const char* variavelChar = variavelStr.c_str();
+      int x = (128 - u8g2.getStrWidth(variavelChar)) / 2;
+      int y = 42;  // Altura central da tela
+      u8g2.drawStr(x, y, variavelChar);
 
-    int x = (128 - u8g2.getStrWidth(variavelChar)) / 2;
+    }
+    if(tela == 1){
+      u8g2.setFont(u8g2_font_inb16_mr);
+    // Centraliza o valor da variável no meio da tela
+    String variavelStr = String(encoderValue);
+    const char* variavelChar = variavelStr.c_str();
+    // Combina as strings da variável e das setas
+    String textoCompleto = variavelStr + "   " + altura ;
+    int x = (128 - u8g2.getStrWidth(textoCompleto.c_str())) / 2;
     int y = 42;  // Altura central da tela
+    u8g2.drawStr(x, y, textoCompleto.c_str());
+    // Desenha uma seta para cima
+    u8g2.setDrawColor(1); // Define a cor do desenho como preto (1)
+    u8g2.setDrawColor(2); // Define a espessura da linha como 2 pixels
     
-    if(tela == true) u8g2.drawStr(x, y, variavelChar);
-    else {
+    if(encoderValue > altura){
+      u8g2.drawLine(53, 30, 63, 40); // Linha diagonal inferior da seta
+      u8g2.drawLine(63, 40, 73, 30); // Linha diagonal superior da seta
+    }
+     if (encoderValue < altura){
+      u8g2.drawLine(53, 40, 63, 30); // Linha diagonal superior invertida da seta
+      u8g2.drawLine(63, 30, 73, 40); // Linha diagonal inferior invertida da seta
+    }
+
+    
+  }
+    if (tela == 2) {
+      u8g2.setFont(u8g2_font_inb30_mr);
+      // Centraliza o valor da variável no meio da tela
+      String variavelStr = String(altura);
+      const char* variavelChar = variavelStr.c_str();
+      int x = (128 - u8g2.getStrWidth(variavelChar)) / 2;
+      int y = 42;  // Altura central da tela
       u8g2.drawStr(x, y, "  ");
-      u8g2.clearBuffer(); // Limpa o buffer
-      u8g2.sendBuffer();  // Envia o buffer limpo para o display
+      u8g2.clearBuffer();  // Limpa o buffer
+      u8g2.sendBuffer();   // Envia o buffer limpo para o display
     }
 }
 
-
 void setup() {
-    /*Serial.begin(115200);
-    while (!Serial);*/
+    //Serial.begin(115200);
+    //while (!Serial)
+       // ;
 
     Serial.println("MPR121 Capacitive Touch Test");
 
     if (!touch.begin()) {
         Serial.println("Error initializing MPR121");
-        while (1);
+        while (1)
+            ;
     }
     touch.setThresholds(2, 0);  // Ajuste de limiar de toque e liberação
 
+    // Configurando os pinos do encoder
+    pinMode(ENCODER_CLK_PIN, INPUT_PULLUP);
+    pinMode(ENCODER_DT_PIN, INPUT_PULLUP);
+
+    encoder.attachHalfQuad(ENCODER_CLK_PIN, ENCODER_DT_PIN);
+
     u8g2.begin();
-    
-        ultimaAlteracao = millis();
+    encoder.setCount(90);
+    ultimaAlteracao = millis();
 }
 
 void loop() {
-    uint16_t currentlyTouched = touch.touched();
+    // Leitura do encoder
+     encoderValue = encoder.getCount();
+
+    // Exibir valores do encoder
+    //Serial.println("Encoder: " + String(encoderValue));
 
     // Verifica se houve uma mudança no estado de toque para qualquer porta
+    uint16_t currentlyTouched = touch.touched();
     if (currentlyTouched != lastTouched) {
-        Serial.print("currentlyTouched: ");
-        Serial.print(currentlyTouched, BIN);
-        Serial.print("  lastTouched: ");
-        Serial.print(lastTouched, BIN);
-        Serial.print(" -  var: ");
-        Serial.println(variavel);
-
+      if (tela == 1){
+        altura = encoderValue;
+      }
+      if(tela == 2){
+        tela = 0;
+      }
         for (uint8_t i = 0; i < 12; i++) {
             // Verifica se a porta atual mudou de estado
             if ((currentlyTouched & (1 << i)) != (lastTouched & (1 << i))) {
                 if (currentlyTouched & (1 << i)) {
-                    tela = true;
+                    tela = 0; desligaTela = millis();
                     // Porta está sendo tocada agora
                     portaTocadaAnteriormente[i] = true;
 
@@ -72,10 +125,12 @@ void loop() {
                         // Transição em outras portas
                         if (portaTocadaAnteriormente[i - 1]) {
                             // Transição para uma porta maior (descendo)
-                            if (variavel > 55) variavel -= 1;
+                            if (altura > 55)
+                                altura -= 1;
                         } else if (portaTocadaAnteriormente[i + 1]) {
                             // Transição para uma porta menor (subindo)
-                            if (variavel < 120) variavel += 1;
+                            if (altura < 120)
+                                altura += 1;
                         }
                     }
                     // Atualiza o tempo da última alteração
@@ -88,17 +143,37 @@ void loop() {
         }
 
         lastTouched = currentlyTouched;
-       
     }
-         u8g2.firstPage();
-         do {
-          draw();
-         } while (u8g2.nextPage());
-    
+    u8g2.firstPage();
+    do {
+        draw();
+    } while (u8g2.nextPage());
 
     // Verifica se passaram 5 segundos desde a última alteração
-    if (millis() - ultimaAlteracao > 2000) {
-        tela = false;
-        Serial.println("Apagar display");
+    if (millis() - ultimaAlteracao > 1000 && encoderValue != altura ) {
+        tela = 1;
+        
     }
+
+    if(encoderValue < altura && tela == 1){
+      Serial.println("Mesa subindo");
+      desligaTela = millis();
+    }
+    if(encoderValue > altura && tela == 1){
+      Serial.println("Mesa descendo");
+      desligaTela = millis();
+    }
+    if(encoderValue == altura && tela == 1){
+      tela = 0;
+      desligaTela = millis();
+      Serial.println("Mesa parada");
+    }
+    if (millis() - desligaTela > 5000 && tela == 0) {
+        tela = 2;
+        Serial.println("Apagando display");
+    }
+    Serial.print(tela);
+    Serial.print("   ");
 }
+
+
